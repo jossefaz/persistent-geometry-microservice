c54833f10729e007f39cbb4e1fa71b622a2143c1
yossefazoulay <azoulayos@protonmail.com>
2020-06-14 12:35:34 +0300

    Init spring boot application

---
+++ src/main/java/com/yossefaz/peristentgeometry/PeristentGeometryApplication.java
@@ -0,0 +1,13 @@
+package com.yossefaz.peristentgeometry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeristentGeometryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PeristentGeometryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yossefaz/peristentgeometry/PeristentGeometryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yossefaz.peristentgeometry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeristentGeometryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
